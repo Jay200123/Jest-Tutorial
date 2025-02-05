a507d2319e8aada82695822d2102edacd65f55cb
Jay200123 <kenken.ayonon@gmail.com>
2025-02-05 21:58:08 +0800

    Add CRUD operations to FakePostAPI for managing posts

--- api/posts.ts
+++ api/posts.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { Posts } from "../interface";
 
 export class FakePostAPI {
   static async getAllPosts() {
@@ -10,6 +11,30 @@ export class FakePostAPI {
 
   static async getPostById(id: number) {
     const res = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${id}`
+    );
+    return res.data;
+  }
+
+  static async createPost(data: Posts) {
+    const res = await axios.post(
+      `https://jsonplaceholder.typicode.com/posts`,
+      data
+    );
+
+    return res.data;
+  }
+
+  static async updatePost(id: number, data: Partial<Posts>) {
+    const res = await axios.patch(
+      "https://jsonplaceholder.typicode.com/posts/${id}",
+      data
+    );
+    return res.data;
+  }
+
+  static async deletePost(id: number) {
+    const res = await axios.delete(
       `https://jsonplaceholder.typicode.com/posts/${id}`
     );
     return res.data;
